cd1446b0ae61504d28c00094c690277817726a86
othniel <othniel.an@gmail.com>
2024-09-14 17:32:25 +0900

    20240914

--- main.js
+++ main.js
@@ -1143,7 +1143,7 @@ var routes = [
   },
   {
     path: "main",
-    loadChildren: () => import("./chunk-NUAAVDNP.js").then((m) => m.MainModule),
+    loadChildren: () => import("./chunk-MMPHOYTB.js").then((m) => m.MainModule),
     canActivate: [canActivate]
   },
   {
@@ -1193,11 +1193,11 @@ var devRoutes = [
   },
   {
     path: "slot",
-    loadChildren: () => import("./chunk-WAJCSBII.js").then((m) => m.SlotModule)
+    loadChildren: () => import("./chunk-WZ4US7MR.js").then((m) => m.SlotModule)
   },
   {
     path: "main",
-    loadChildren: () => import("./chunk-NUAAVDNP.js").then((m) => m.MainModule)
+    loadChildren: () => import("./chunk-MMPHOYTB.js").then((m) => m.MainModule)
   },
   {
     path: "chatbot",
