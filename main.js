8254315c313e7613a4a64ba387f1469ec78f67f6
niel <othniel.an@gmail.com>
2024-10-24 16:34:39 +0900

    20241024_1

--- main.js
+++ main.js
@@ -1143,7 +1143,7 @@ var routes = [
   },
   {
     path: "main",
-    loadChildren: () => import("./chunk-NQ7A7WAQ.js").then((m) => m.MainModule),
+    loadChildren: () => import("./chunk-ICWRZBHR.js").then((m) => m.MainModule),
     canActivate: [canActivate]
   },
   {
@@ -1193,11 +1193,11 @@ var devRoutes = [
   },
   {
     path: "slot",
-    loadChildren: () => import("./chunk-OMUTE6UV.js").then((m) => m.SlotModule)
+    loadChildren: () => import("./chunk-SMCH4XEI.js").then((m) => m.SlotModule)
   },
   {
     path: "main",
-    loadChildren: () => import("./chunk-NQ7A7WAQ.js").then((m) => m.MainModule)
+    loadChildren: () => import("./chunk-ICWRZBHR.js").then((m) => m.MainModule)
   },
   {
     path: "chatbot",
